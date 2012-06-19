82fb9558b2c2d9e38af043b3345b29dfa352be4e
GABRI Mate <gabrimate@duosol.hu>
2012-06-19 15:53:48 +0200

    Feature #225 radius generation on True or False

--- afs/services/radius.py
+++ afs/services/radius.py
@@ -7,12 +7,13 @@ class RADIUS(ServiceBase):
 		
 		for host in sorted(self.network.keys()):
 			if self.network[host].has_key('radius'):
-				template = """
+				if self.network[host]['radius'] == 1 or self.network[host]['radius'] == True:
+					template = """
 #%s
 %s
 \tReply-Message = "Device with MAC Address %%{User-Name} authorized for network access"
 """ % ( host, self.network[host]['mac'] )
 			
-				output.write(template)
+					output.write(template)
 		
 		output.close()
