00b4bbac7e03a34befc5658831311218bd479731
GABRI Mate <gabrimate@duosol.hu>
2013-01-08 12:38:13 +0100

    Feature #183 os.system changed to subprocess

--- afs/services/__init__.py
+++ afs/services/__init__.py
@@ -1,4 +1,7 @@
+import subprocess
 import os
+from subprocess import CalledProcessError
+from afs.errors import Error
 
 __all__ = [ "dhcp", "dns", "radius", "shorewall" ]
 
@@ -9,4 +12,7 @@ class ServiceBase(object):
 		self.options = config.options[self.__class__.__name__.lower()]
 
 	def reload_service(self):
-		os.system(self.options['reload'])
+		try:
+			subprocess.check_call(self.options['reload'].split(), stdout=open(os.devnull, 'w'))
+		except CalledProcessError:
+			raise Error("Reload of %s returned with error!" % self.__class__.__name__)
